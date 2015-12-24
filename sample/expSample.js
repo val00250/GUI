a79fc684bbc326a01b3eac202d519647800354d2
val00250 <takeda@val.co.jp>
2015-12-24 14:36:38 +0900

    サンプル追加

--- sample/expSample.js
+++ sample/expSample.js
@@ -7,6 +7,12 @@ var stationApp5;// 駅名入力パーツ#5
 var stationApp6;// 駅名入力パーツ#6
 var conditonApp;// 探索条件パーツ
 var resultApp;// 経路表示パーツ
+var repaymentApp;// 定期払い戻しパーツ
+var stationInfoApp;// 駅情報パーツ
+var railApp;//路線情報パーツ
+
+var refreshList = new Array();
+var refreshIndex = 0;
 
 /**
 * パーツを初期化
@@ -151,8 +157,44 @@ function init() {
             document.getElementById("passRoute").value = getCookie("passRoute");
         }
     }
-    // デモ用のコード
-    stationApp1.setStation("高円寺");
+    // 払い戻し計算初期化
+    if (document.getElementById("repayment")) {
+        // 払い戻しのための日付を設定
+        var now = new Date();
+        document.getElementById("repay_start").value = String(now.getFullYear()) + (now.getMonth() < 9 ? "0" : "") + String(now.getMonth() + 1) + (now.getDate() < 10 ? "0" : "") + String(now.getDate());
+        document.getElementById("repay_repayment").value = String(now.getFullYear()) + (now.getMonth() < 9 ? "0" : "") + String(now.getMonth() + 1) + (now.getDate() < 10 ? "0" : "") + String(now.getDate());
+        repaymentApp = new expGuiRepayment(document.getElementById("repayment"));
+        if (typeof apiURL != 'undefined') {
+            repaymentApp.setConfigure("apiURL", apiURL);
+        }
+        if (typeof key != 'undefined') {
+            repaymentApp.setConfigure("key", key);
+        }
+        if (typeof initRepayment != 'undefined') {
+            initRepayment();
+        }
+    }
+
+    stationInfoApp = new expGuiStationInfo();
+    if (typeof apiURL != 'undefined') {
+        stationInfoApp.setConfigure("apiURL", apiURL);
+    }
+    if (typeof key != 'undefined') {
+        stationInfoApp.setConfigure("key", key);
+    }
+    if (typeof initRepayment != 'undefined') {
+        initStationInfo();
+    }
+    railApp = new expGuiRail();
+    if (typeof apiURL != 'undefined') {
+        railApp.setConfigure("apiURL", apiURL);
+    }
+    if (typeof key != 'undefined') {
+        railApp.setConfigure("key", key);
+    }
+    if (typeof initRepayment != 'undefined') {
+        initRail();
+    }
 }
 
 /**
@@ -284,8 +326,11 @@ function checkData() {
             viaList.push(stationApp6.getStation());
         }
     }
-    if (viaList.length == 0 || viaList.length == 1) {
-        errorMessage += "\n出発地・目的地は必須です。";
+    if (viaList.length == 0) {
+        errorMessage += "\n出発地は必須です。";
+    }
+    if (viaList.length == 1) {
+        errorMessage += "\n目的地は必須です。";
     } else {
         if (viaList.length == 2) {
             if (viaList[0] == viaList[1]) {
@@ -316,8 +361,6 @@ function checkData() {
 */
 function search(callBack) {
     setCookie();
-    // デモ用のコード
-    stationApp1.setStation("高円寺");
     // 入力チェック後に動作
     if (checkData()) {
         var searchWord = "";
@@ -408,6 +451,217 @@ function search(callBack) {
 }
 
 /**
+* 定期文字列から経路探索を行う
+*/
+function restoreTeikiRoute(callBack) {
+    setCookie();
+    var passRoute = document.getElementById("passRoute").value;
+    if (passRoute == "") {
+        alert("下記の項目を確認してください。\n定期区間文字列を正しく設定してください。");
+        return false;
+    }
+    var detailFlag = false;
+    var passRouteList = passRoute.split(":");
+    var viaList = new Array();
+    var fixedRailList = new Array();
+    var fixedRailDirectionList = new Array();
+    var count = 0;
+    while (count < passRouteList.length) {
+        // 駅
+        viaList.push(passRouteList[count]);
+        count++;
+        if (count == passRouteList.length) {
+            // 目的地の場合は終了
+            break;
+        }
+        // 路線
+        fixedRailList.push(passRouteList[count]);
+        count++;
+        //方向
+        if (passRouteList[count].toLowerCase() == "up") {
+            fixedRailDirectionList.push("up");
+            detailFlag = true;
+            count++;
+        } else if (passRouteList[count].toLowerCase() == "down") {
+            fixedRailDirectionList.push("down");
+            detailFlag = true;
+            count++;
+        }
+    }
+    // パラメータの組み立て
+    var searchWordList = new Array();
+    searchWordList.push('&date=' + dateTimeApp.getDate());
+    searchWordList.push("searchType=plain");
+    searchWordList.push("viaList=" + viaList.join(":"));
+    searchWordList.push("fixedRailList=" + fixedRailList.join(":"));
+    if (detailFlag) {
+        searchWordList.push("fixedRailDirectionList=" + fixedRailDirectionList.join(":"));
+    }
+
+    // コールバックの設定
+    if (typeof callBack != 'undefined') {
+        resultApp.bind("select", callBack);
+    } else {
+        resultApp.unbind("select");
+    }
+    // ソート
+    searchWordList.push('sort=' + conditonApp.getSortType());
+    // 探索結果数
+    searchWordList.push('answerCount=' + conditonApp.getAnswerCount());
+    // 探索条件
+    searchWordList.push('conditionDetail=' + conditonApp.getConditionDetail());
+    // 会社名の出力をデフォルトにする
+    searchWordList.push("resultDetail=addCorporation");
+
+    // 探索を実行
+    resultApp.search(searchWordList.join("&"), resultApp.PRICE_TEIKI, repaymentCalc);
+}
+
+/**
+* 継続更新を行う
+*/
+function routeRefresh(callBack) {
+    setCookie();
+    var passRoute = document.getElementById("passRoute").value;
+    if (passRoute == "") {
+        alert("下記の項目を確認してください。\n定期区間文字列を正しく設定してください。");
+        return false;
+    }
+    refreshList = new Array();
+    var detailFlag = false;
+    var passRouteList = passRoute.split(":");
+    var count = 0;
+    while (count < passRouteList.length) {
+        // 駅
+        var tmp_obj = new Object();
+        tmp_obj.type = "station";
+        tmp_obj.name = passRouteList[count];
+        tmp_obj.refresh = false;
+        refreshList.push(tmp_obj);
+        count++;
+        if (count == passRouteList.length) {
+            // 目的地の場合は終了
+            break;
+        }
+        // 路線
+        var tmp_obj = new Object();
+        tmp_obj.type = "rail";
+        tmp_obj.name = passRouteList[count];
+        tmp_obj.refresh = false;
+        refreshList.push(tmp_obj);
+        count++;
+        //方向
+        if (passRouteList[count].toLowerCase() == "up") {
+            var tmp_obj = new Object();
+            tmp_obj.type = "direction";
+            tmp_obj.name = "up";
+            tmp_obj.refresh = true;
+            refreshList.push(tmp_obj);
+            count++;
+        } else if (passRouteList[count].toLowerCase() == "down") {
+            var tmp_obj = new Object();
+            tmp_obj.type = "direction";
+            tmp_obj.name = "down";
+            tmp_obj.refresh = true;
+            refreshList.push(tmp_obj);
+            count++;
+        }
+    }
+    // 継続更新を実行
+    refreshStart();
+}
+
+/**
+* 継続更新処理
+*/
+function refreshStart() {
+    for (var i = 0; i < refreshList.length; i++) {
+        if (!refreshList[i].refresh) {
+            refreshIndex = i;
+            if (refreshList[i].type == "station") {
+                stationInfoApp.getStationOldName(refreshList[i].name, setStationName);
+                return;
+            } else if (refreshList[i].type == "rail") {
+                railApp.getRailOldName(refreshList[i].name, setRailName);
+                return;
+            }
+        }
+    }
+    // 更新後の表示
+    var buffer = "";
+    for (var i = 0; i < refreshList.length; i++) {
+        if (i != 0) { buffer += ":"; }
+        buffer += refreshList[i].name;
+    }
+    document.getElementById("passRoute").value = buffer;
+}
+
+/**
+* 駅名を取得する
+*/
+function setStationName(isSuccess) {
+    if (!isSuccess) {
+        refreshList[refreshIndex].refresh = true;
+        refreshStart();
+    } else {
+        var tmp_stationList = stationInfoApp.getStationList().split(",");
+        refreshList[refreshIndex].name = tmp_stationList[0];
+        refreshList[refreshIndex].refresh = true;
+        refreshStart();
+    }
+}
+
+/**
+* 路線名を取得する
+*/
+function setRailName(isSuccess) {
+    if (!isSuccess) {
+        refreshList[refreshIndex].refresh = true;
+        refreshStart();
+    } else {
+        var tmp_railList = railApp.getRailList().split(",");
+        refreshList[refreshIndex].name = tmp_railList[0];
+        refreshList[refreshIndex].refresh = true;
+        refreshStart();
+    }
+}
+
+/**
+* 払い戻し計算開始
+*/
+function repaymentCalc(isSuccess){
+    if (isSuccess) {
+        // 探索結果が取得できているかを確認
+        if (resultApp.getResultCount() >= 1) {
+            // インターフェースを作成
+            var repaymentIf = repaymentApp.createRepaymentInterface();
+            // シリアライズデータのセット
+            repaymentIf.setSerializeData(resultApp.getSerializeData());
+            // 開始日
+            repaymentIf.setStartDate(document.getElementById("repay_start").value);
+            // 払戻日
+            repaymentIf.setRepaymentDate(document.getElementById("repay_repayment").value);
+            // 有効期間
+            if (document.getElementById("repay_validity").options.item(document.getElementById("repay_validity").selectedIndex).value == "1") {
+                repaymentIf.setValidityPeriod(repaymentApp.TEIKI1);
+            } else if (document.getElementById("repay_validity").options.item(document.getElementById("repay_validity").selectedIndex).value == "3") {
+                repaymentIf.setValidityPeriod(repaymentApp.TEIKI3);
+            } else if (document.getElementById("repay_validity").options.item(document.getElementById("repay_validity").selectedIndex).value == "6") {
+                repaymentIf.setValidityPeriod(repaymentApp.TEIKI6);
+            }
+            // 区間変更
+            if (document.getElementById("repay_change").options.item(document.getElementById("repay_change").selectedIndex).value == "1") {
+                repaymentIf.setChangeSection(true);
+            } else {
+                repaymentIf.setChangeSection(false);
+            }
+            // 払い戻し計算
+            repaymentApp.dispRepayment(repaymentIf);
+        }
+    }
+}
+
+/**
 * 探索実行時のコールバック関する
 */
 function result(isSuccess) {
@@ -469,8 +723,7 @@ function getCookie(name) {
         result = decodeURIComponent(
         allcookies.substring(startIndex, endIndex));
     }
-    // デモ用のコード
-    return "";
+    return result;
 }
 
 /**
